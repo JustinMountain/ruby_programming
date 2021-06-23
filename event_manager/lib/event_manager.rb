8bfe9be4884bc335bc42f92ce3a34b6ee4a7a672
Justin Mountain <mountainj@gmail.com>
2021-06-23 17:23:00 -0300

    Prep done; ready for assignments

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,31 +1,37 @@
 require 'csv'
-
-puts 'EventManager initialized.'
-
-# contents = File.read('event_attendees.csv')
-# puts contents
-
-# lines = File.readlines('event_attendees.csv')
-# lines.each_with_index do |line, index|
-#   next if index == 0
-#   columns = line.split(",")
-#   name = columns[2]
-#   p name
-# end
+require "google/apis/civicinfo_v2"
+require 'erb'
 
 def clean_zipcode(zipcode)
-  # if zipcode.nil?
-  #   zipcode = "00000"
-  # elsif zipcode.length < 5
-  #   # until zipcode.length == 5
-  #   #   zipcode.prepend("0")
-  #   # end
-  #   zipcode = zipcode.rjust(5, "0")
-  # else zipcode.length > 5
-  #   zipcode = zipcode[0..4]
-  # end
   zipcode.to_s.rjust(5, "0")[0..4]
 end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'LegislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thankyou_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exists?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager initialized.'
 
 contents = CSV.open(
   'event_attendees.csv', 
@@ -33,9 +39,17 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
-  puts "#{name} #{zipcode}"
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thankyou_letter(id, form_letter)
 end

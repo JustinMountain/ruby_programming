21a88df20d73c15c75016c265e6e4ef4ec4c1464
Justin Mountain <mountainj@gmail.com>
2021-07-05 18:44:19 -0300

    Merge Sorted

--- recursion/README.md
+++ recursion/README.md
@@ -1,3 +1,7 @@
+2021-07-05 Post Mortem
+- 
+
 2021-07-05
+- Merge Sorted!
 - Iterative and Recursive fibonacci completed
 - Directory and files established
--- recursion/recursion.rb
+++ recursion/recursion.rb
@@ -23,6 +23,34 @@ end
 
 p fibs_rec(10)
 
-def merge_sort
-  # code goes here
+def merge_sort(array)
+  # Base Case
+  return array if array.length < 2
+
+  half = array.length / 2
+  left = merge_sort(array.slice(0...half))
+  right = merge_sort(array.slice(half..-1))
+
+  combine(left, right)
 end
+
+def combine(left_array, right_array)
+  merge = []
+  left_array = left_array
+  right_array = right_array
+
+  until left_array.empty? || right_array.empty?
+    if left_array.first < right_array.first
+      merge << left_array.shift
+    elsif right_array.first < left_array.first
+      merge << right_array.shift
+    else
+      merge << left_array.shift
+      merge << right_array.shift
+    end
+  end
+  
+  return merge + left_array + right_array
+end
+
+p merge_sort([5, 2, 1, 3, 6, 4, 3, 9, 7, 1, 8, 10])

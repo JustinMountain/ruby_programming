643b1cabe176c76228ecc8975b52f7f09569462a
Justin Mountain <mountainj@gmail.com>
2021-07-10 12:28:19 -0300

    Find and Level_Order methods added

--- binary_search_trees/README.md
+++ binary_search_trees/README.md
@@ -1,3 +1,8 @@
+2021-07-10
+- Implemented level_order method that returns an array of values in level order
+  - Added case for clearing array if the method is envoked more than once
+- Implemented the find method
+
 2021-07-09
 - Recursive delete implemented by translating code from C# (iterative approach can be found in history)
   - https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/?ref=lbp
--- binary_search_trees/binary_search_trees.rb
+++ binary_search_trees/binary_search_trees.rb
@@ -8,11 +8,12 @@ class Node
 end
 
 class Tree
-  attr_accessor :array, :my_root, :unsorted, :sorted
+  attr_accessor :array, :my_root, :unsorted, :sorted, :level_order_array
   def initialize(array)
     @unsorted = array
     @sorted = array.sort!.uniq
     @my_root = build_tree(@sorted)
+    @level_order_array = []
   end
 
   def build_tree(array)
@@ -70,6 +71,33 @@ class Tree
     minv
   end
 
+  def find(value, node=@my_root)
+    # Base Case
+    return node if node.value == value || node.nil?
+    node.value > value ? find(value, node.left) : find(value, node.right)
+  end
+
+  def level_order(queue=[], node=@my_root)
+    if @level_order_array.length > 0
+      @level_order_array = []
+    end
+
+    level_order_recursion(queue, node)
+  end
+
+  def level_order_recursion(queue, node)
+    @level_order_array.push(node.value)
+
+    # Base Case
+    return @level_order_array if queue.length == 0 && node.left == nil && node.right == nil
+
+    # Record current node's children to the queue
+    queue.push(node.left) unless node.left.nil?
+    queue.push(node.right) unless node.right.nil?
+    # Recursively work through the queue
+    level_order_recursion(queue, queue.shift)    
+  end
+
   # Pretty Print method from the project
   def pretty_print(node = @my_root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
@@ -95,15 +123,21 @@ my_tree.pretty_print
 
 # my_tree.pretty_print
 
-# Delete Testing
-  # 0 children
-my_tree.delete(5)
+# # Delete Testing
+#   # 0 children
+# my_tree.delete(5)
 
-  # 1 child
-my_tree.delete(23)
+#   # 1 child
+# my_tree.delete(23)
 
-  # 2 children
-my_tree.delete(67)
+#   # 2 children
+# my_tree.delete(67)
 
-my_tree.pretty_print
+# my_tree.pretty_print
 
+# # Find testing
+# puts my_tree.find(324)
+# puts my_tree.find(67)
+
+# Level Order Array Testing
+p my_tree.level_order

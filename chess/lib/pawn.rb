c773f72762dff9fb9674c18e15e3cddb6326b2f5
Justin Mountain <mountainj@gmail.com>
2021-08-08 16:38:44 -0300

    Work on Pawn to include direction-limited movement

--- chess/lib/pawn.rb
+++ chess/lib/pawn.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
+require_relative '../lib/piece'
+
 # Object to control pawn movement
-class Pawn
+class Pawn < Piece
   # Determines the valid moves for a knight at position start_location
   def valid_moves(start, board)
     if start.is_a?(Array) && start.length == 2
-      all_moves = all_moves(start)
-      directed = player_direction(all_moves, start, board)
-      player_adjusted = player_correction(directed, start, board)
-      possible = possible_moves(player_adjusted)
+      player = board.board[row][column].owner
+
+      directed_moves = determine_movement(player)
+      all_moves = all_moves(start, directed_moves)
+
+      player_corrected = player_correction(all_moves, player)
+      possible = possible_moves(player_corrected)
       player_correction(start, possible, board)
     else
       'Error'
@@ -16,12 +21,11 @@ class Pawn
   end
 
   # Used in valid_moves to determine all potential moves
-  def all_moves(start)
+  def all_moves(start, directed_moves)
     # Array of valid moves
-    valid_moves = [[1, 0], [1, 1], [1, -1]]
     all_moves = []
 
-    valid_moves.each do |ele|
+    directed_moves.each do |ele|
       ele[0] = ele[0] + start[0]
       ele[1] = ele[1] + start[1]
       all_moves << ele
@@ -29,26 +33,14 @@ class Pawn
     all_moves
   end
 
-  # Determines the direction pawns are allowed to move
-  def player_direction(all_moves, start, board)
-    row = start[0]
-    column = start[1]
-    player = board.board[row][column].owner
-
-    all_moves unless player == 'Player2'
-
-    all_moves.each { |pos| pos[0] *= -1 }
-    all_moves
-  end
-
-  # Used in valid_moves to limit potential moves to be on the board
-  def possible_moves(all_moves)
-    possible_moves = []
-
-    all_moves.each do |ele|
-      possible_moves << ele unless ele[0] > 8 || ele[1] > 8 || ele[0] < 1 || ele[1] < 1
+  def determine_movement(player)
+    case player
+    when 'Player1'
+      moves = [[1, -1], [1, 0], [1, 1]]
+    when 'Player2'
+      moves = [[-1, -1], [-1, 0], [-1, 1]]
     end
-    possible_moves
+    moves
   end
 
   # Used in valid_moves to limit potential moves to not take own piece
@@ -66,55 +58,5 @@ class Pawn
       end
     end
     corrected
-  end
-
-  # Used in player_correction
-  def node_owner(location, board)
-    row = location[0]
-    column = location[1]
-    board.board[row][column].owner
-  end
-
-  # Handles updating the gameboard when moving the pawn
-  def move(start, finish, board)
-    if start.is_a?(Integer) || start.length != 2
-      'Invalid start location'
-    elsif !finish.is_a?(Array) || finish.length != 2
-      'Invalid finish location'
-    else
-      check_validity(start, finish, board)
-    end
-  end
-
-  # Used in #move
-  def update_start(start, board)
-    start_row = start[0]
-    start_column = start[1]
-    start_node = board.board[start_row][start_column]
-    start_node.reset_node
-  end
-
-  # Used in #move
-  def update_finish(finish, board, player)
-    finish_row = finish[0]
-    finish_column = finish[1]
-    finish_node = board.board[finish_row][finish_column]
-    finish_node.pawn(player)
-  end
-
-  # Used in #move
-  def check_validity(start, finish, board)
-    valid = valid_moves(start)
-    if valid.include?(finish)
-      start_owner = node_owner(start, board)
-      finish_owner = node_owner(finish, board)
-
-      if finish_owner != start_owner
-        update_start(start, board)
-        update_finish(finish, board, start_owner)
-      end
-    else
-      'Invalid move'
-    end
   end
 end
---
+++ chess/spec/pawn_spec.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+require_relative '../lib/pawn'
+
+RSpec.describe Pawn do
+  describe '#valid_moves' do
+    let(:test_pawn) { described_class.new }
+
+    context 'an invalid start location' do
+      it "should return a string containing 'Error'" do
+        invalid_start = [2, 2, 2]
+        board = instance_double('board')
+
+        error_condition = test_pawn.valid_moves(invalid_start, board)
+        error_message = 'Error'
+
+        expect(error_condition).to eq(error_message)
+      end
+    end
+  end
+
+  describe '#all_moves' do
+    let(:test_pawn) { described_class.new }
+
+    context 'there is a pawn at 1, 1 for player 1' do
+      it 'should return an array of 3 move locations' do
+        start_location = [1, 1]
+        directed_moves = [[1, -1], [1, 0], [1, 1]]
+        all_moves = test_pawn.all_moves(start_location, directed_moves)
+
+        expect(all_moves.length).to be(3)
+      end
+
+      it 'should return [2, 0] for the first move location [0]' do
+        start_location = [1, 1]
+        directed_moves = [[1, -1], [1, 0], [1, 1]]
+        all_moves = test_pawn.all_moves(start_location, directed_moves)
+        first_move = all_moves[0]
+        expected = [2, 0]
+
+        expect(first_move).to eq(expected)
+      end
+
+      it 'should return [2, 2]for the third move location [2]' do
+        start_location = [1, 1]
+        directed_moves = [[1, -1], [1, 0], [1, 1]]
+        all_moves = test_pawn.all_moves(start_location, directed_moves)
+        third_move = all_moves[2]
+        expected = [2, 2]
+
+        expect(third_move).to eq(expected)
+      end
+    end
+
+    context 'there is a pawn at 5, 5 for player 2' do
+      it 'should return an array of 3 move locations' do
+        start_location = [5, 5]
+        directed_moves = [[1, -1], [1, 0], [1, 1]]
+        all_moves = test_pawn.all_moves(start_location, directed_moves)
+
+        expect(all_moves.length).to be(3)
+      end
+
+      it 'should return [6, 4] for the first move location [0]' do
+        start_location = [5, 5]
+        directed_moves = [[1, -1], [1, 0], [1, 1]]
+        all_moves = test_pawn.all_moves(start_location, directed_moves)
+        first_move = all_moves[0]
+        expected = [6, 4]
+
+        expect(first_move).to eq(expected)
+      end
+
+      it 'should return [6, 6] for the third move location [2]' do
+        start_location = [5, 5]
+        directed_moves = [[1, -1], [1, 0], [1, 1]]
+        all_moves = test_pawn.all_moves(start_location, directed_moves)
+        third_move = all_moves[2]
+        expected = [6, 6]
+
+        expect(third_move).to eq(expected)
+      end
+    end
+  end
+
+  describe '#determine movement' do
+    let(:test_pawn) { described_class.new }
+
+    it 'should a return positive array for Player1' do
+      player = 'Player1'
+      expected_array = [[1, -1], [1, 0], [1, 1]]
+
+      determined = test_pawn.determine_movement(player)
+
+      expect(determined).to eq(expected_array)
+    end
+
+    it 'should a return positive array for Player2' do
+      player = 'Player2'
+      expected_array = [[-1, -1], [-1, 0], [-1, 1]]
+
+      determined = test_pawn.determine_movement(player)
+
+      expect(determined).to eq(expected_array)
+    end
+  end
+
+  describe '#player_correction' do
+    let(:test_pawn) { described_class.new }
+
+    context 'there is a player piece at the intended location'
+    context 'there is no player piece at any intended location'
+  end
+end

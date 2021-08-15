f24666f46ae899d697cb436b6e930cbb5a847033
Justin Mountain <mountainj@gmail.com>
2021-08-15 17:00:14 -0300

    Piece will limit possible moves to be on the board

--- chess_v2/lib/piece.rb
+++ chess_v2/lib/piece.rb
@@ -2,15 +2,54 @@
 
 # Piece to be used for all Game Pieces
 class Piece
-  attr_reader :player, :location
+  attr_reader :player, :location, :moves_array
 
   def initialize(player, location)
     @player = player
     @location = location
+    @moves_array = [[-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, 2], [2, 1], [2, -1], [1, -2]]
   end
 
   def move(board, start, finish)
     board.reset_location_marker(start)
     board.update_board(finish)
   end
+
+  # Add the start location to this array
+  def moves_from_start(start)
+    start_adjusted = @moves_array
+
+    start_adjusted.each do |ele|
+      ele[0] = ele[0] + start[0]
+      ele[1] = ele[1] + start[1]
+    end
+    start_adjusted
+  end
+
+  # Remove any coordinates that aren't on the board (position greater than 8 or less than 1)
+  def remove_invalid_coordinates(start_adjusted_moves)
+    board_adjusted = []
+
+    start_adjusted_moves.each do |ele|
+      board_adjusted << ele unless ele[0] > 7 || ele[1] > 7 || ele[0] < 0 || ele[1] < 0
+    end
+    board_adjusted
+  end
+
+  # Remove any square with player's own pieces in it
+  def remove_own_piece_locations(board, board_adjusted_moves)
+  end
+
+  # Remove any piece-specific restricted positions
+  def piece_specific_restrictions(own_piece_adjusted_moves)
+  end
+
+  # def valid_move?(start, finish)
+  #   start_adjusted_moves = moves_from_start(start)
+  #   board_adjusted_moves = remove_invalid_coordinates(start_adjusted_moves)
+  #   own_piece_adjusted_moves = remove_own_piece_locations(board_adjusted_moves)
+  #   piece_rectricted = piece_specific_restrictions(own_piece_adjusted_moves)
+  #   true if piece_rectricted.include(finish)
+  #   false
+  # end
 end
--- chess_v2/spec/piece_spec.rb
+++ chess_v2/spec/piece_spec.rb
@@ -17,4 +17,70 @@ RSpec.describe Piece do
       piece.move(board_double, start, finish)
     end
   end
+
+  describe '#moves_from_start' do
+    let(:piece) { described_class.new('Player1', [1, 1]) }
+
+    it 'returns an array of length 8 for a piece starting at any board location' do
+      start = piece.location
+      start_adjusted_moves = piece.moves_from_start(start)
+
+      expect(start_adjusted_moves.length).to eq(8)
+    end
+
+    it 'has [0, 3] as the first array index' do
+      start = piece.location
+      start_adjusted_moves = piece.moves_from_start(start)
+
+      expect(start_adjusted_moves[0]).to eq([0, 3])
+    end
+
+    it 'has [0, -1] as the fourth array index' do
+      start = piece.location
+      start_adjusted_moves = piece.moves_from_start(start)
+
+      expect(start_adjusted_moves[3]).to eq([0, -1])
+    end
+
+    it 'has [2, -1] as the last array index' do
+      start = piece.location
+      start_adjusted_moves = piece.moves_from_start(start)
+
+      expect(start_adjusted_moves[7]).to eq([2, -1])
+    end
+  end
+
+  describe '#remove_invalid_coordinates' do
+    context 'the piece starts at [1, 1]' do
+      let(:piece) { described_class.new('Player1', [1, 1]) }
+
+      it 'removes all row and column positions less than 0' do
+        start_adjusted_moves = [[0, 3], [-1, 2], [-1, 0], [0, -1], [2, 3], [3, 2], [3, 0], [2, -1]]
+        expected_return = [[0, 3], [2, 3], [3, 2], [3, 0]]
+
+        board_adjusted_moves = piece.remove_invalid_coordinates(start_adjusted_moves)
+
+        expect(board_adjusted_moves).to eq(expected_return)
+      end
+    end
+
+    context 'the piece starts at [7, 7]' do
+      let(:piece) { described_class.new('Player1', [7, 7]) }
+
+      it 'removes all row and column positions greater than 7' do
+        start_adjusted_moves = [[7, 10], [6, 9], [6, 7], [7, 6], [9, 10], [10, 9], [10, 7], [9, 6]]
+
+        expected_return = [[6, 7], [7, 6]]
+
+        board_adjusted_moves = piece.remove_invalid_coordinates(start_adjusted_moves)
+
+        expect(board_adjusted_moves).to eq(expected_return)
+      end
+    end
+  end
+
+  describe '#remove_own_piece_locations' do
+    let(:piece) { described_class.new('Player1', [1, 1]) }
+
+  end
 end

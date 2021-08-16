146754db19a5e9c37e323fa0280c792066514716
Justin Mountain <mountainj@gmail.com>
2021-08-16 17:58:05 -0300

    Rook movement restrictions complete. Needs refactoring

--- chess_v2/lib/rook.rb
+++ chess_v2/lib/rook.rb
@@ -25,15 +25,46 @@ class Rook < Piece
     end
   end
 
-  def piece_specific_restrictions(_board, own_piece_adjusted_moves)
-    own_piece_adjusted_moves
+  def piece_specific_restrictions(board, own_piece_adjusted_moves)
+    current_board = board.return_board
+
+    adjusted = []
+    return_array = []
+
+    directed_movement = [
+      [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]],
+      [[-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]],
+      [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]],
+      [[0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7]]
+    ]
+
+    directed_movement.each do |direction|
+      direction.each do |position|
+        adjusted[0] = position[0] + @location[0]
+        adjusted[1] = position[1] + @location[1]
+
+        if own_piece_adjusted_moves.include?([adjusted[0], adjusted[1]])
+          location_marker = current_board[adjusted[0]][adjusted[1]]
+
+          case player
+          when 'Player1'
+            opponent_pieces = location_marker.match(/[\u2654-\u2659]/)
+          when 'Player2'
+            opponent_pieces = location_marker.match(/[\u265A-\u265F]/)
+          end
+
+          if opponent_pieces
+            return_array << [adjusted[0], adjusted[1]]
+            break
+          elsif location_marker == "\u00B7"
+            return_array << [adjusted[0], adjusted[1]]
+          end
+        else
+          break
+        end
+      end
+    end
+
+    return_array
   end
 end
-
-rook = Rook.new('Player1', [1, 1])
-
-start_adjusted_moves = rook.moves_from_start(rook.location)
-print start_adjusted_moves
-puts ''
-board_adjusted_moves = rook.remove_invalid_coordinates(start_adjusted_moves)
-print board_adjusted_moves

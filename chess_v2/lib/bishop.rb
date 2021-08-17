9e0f47e5f412cd84e16d9bf6be91364a7e09467d
Justin Mountain <mountainj@gmail.com>
2021-08-17 17:44:24 -0300

    Bishop object and tests complete

--- chess_v2/lib/bishop.rb
+++ chess_v2/lib/bishop.rb
@@ -10,13 +10,80 @@ class Bishop < Piece
     super
     @player = player
     @location = location
-    @moves_array = []
+    @moves_array = [
+      # top-left to bottom-right
+      [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+      [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7],
+      # bottom-left to top-right
+      [1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7],
+      [-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]
+    ]
+  end
 
-    case player
+  def update_marker
+    case @player
     when 'Player1'
       @marker = "\u265D"
     when 'Player2'
       @marker = "\u2657"
     end
+  end
+
+  def piece_specific_restrictions(board, own_piece_adjusted_moves)
+    current_board = board.return_board
+    restricted_array = []
+
+    directed_movement = [
+      [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]],
+      [[-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7]],
+      [[1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7]],
+      [[-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]]
+    ]
+
+    directed_movement.each do |direction|
+      direction_limited(current_board, direction, own_piece_adjusted_moves, restricted_array)
+    end
+
+    restricted_array
+  end
+
+  private
+
+  def direction_limited(current_board, direction, own_piece_adjusted_moves, restricted_array)
+    direction.each do |position|
+      adjusted = adjust_position(position)
+
+      break unless own_piece_adjusted_moves.include?([adjusted[0], adjusted[1]])
+
+      location_marker = current_board[adjusted[0]][adjusted[1]]
+
+      opponent_piece = opponent_occupies?(location_marker)
+
+      restricted_array << [adjusted[0], adjusted[1]] if opponent_piece || location_marker == "\u00B7"
+
+      break if opponent_piece
+    end
+    restricted_array
+  end
+
+  def adjust_position(position)
+    adjusted = []
+
+    adjusted[0] = position[0] + @location[0]
+    adjusted[1] = position[1] + @location[1]
+
+    adjusted
+  end
+
+  def opponent_occupies?(location_marker)
+    opponent_piece = false
+
+    case @player
+    when 'Player1'
+      opponent_piece = true if location_marker.match(/[\u2654-\u2659]/)
+    when 'Player2'
+      opponent_piece = true if location_marker.match(/[\u265A-\u265F]/)
+    end
+    opponent_piece
   end
 end
--- chess_v2/spec/bishop_spec.rb
+++ chess_v2/spec/bishop_spec.rb
@@ -7,12 +7,27 @@ RSpec.describe Bishop do
     context 'Player 1' do
       let(:bishop) { described_class.new('Player1', [1, 1]) }
 
-      # it 'should store the bishop movement array' do
-      #   expected_array = []
-      #   returned_array = bishop.moves_array
+      it 'should store the bishop movement array' do
+        expected_array = [
+          [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+          [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-6, -6], [-7, -7],
+          [1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7],
+          [-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-6, 6], [-7, 7]
+        ]
+        returned_array = bishop.moves_array
 
-      #   expect(returned_array).to eq(expected_array)
-      # end
+        expect(returned_array).to eq(expected_array)
+      end
+    end
+  end
+
+  describe '#update_marker' do
+    context 'Player 1' do
+      let(:bishop) { described_class.new('Player1', [1, 1]) }
+
+      before do
+        bishop.update_marker
+      end
 
       it 'should store the marker appropriate for Player 1' do
         expected_marker = "\u265D"
@@ -25,11 +40,148 @@ RSpec.describe Bishop do
     context 'Player 2' do
       let(:bishop) { described_class.new('Player2', [1, 1]) }
 
+      before do
+        bishop.update_marker
+      end
+
       it 'should store the marker appropriate for Player 2' do
         expected_marker = "\u2657"
         returned_marker = bishop.marker
 
         expect(returned_marker).to eq(expected_marker)
+      end
+    end
+  end
+
+  describe '#piece_specific_restrictions' do
+    let(:bishop) { described_class.new('Player1', [1, 1]) }
+
+    context 'there is an empty game board' do
+      it 'returns 9 possible move locations' do
+        empty_board = [
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u265D", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"]
+        ]
+        board_double = double('board_double', return_board: empty_board)
+        own_piece_adjusted_moves = [
+          [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+          [0, 0],
+          [2, 0],
+          [0, 2]
+        ]
+        returned_array = bishop.piece_specific_restrictions(board_double, own_piece_adjusted_moves)
+
+        expect(returned_array).to eq(own_piece_adjusted_moves)
+        expect(returned_array.length).to eq(9)
+      end
+    end
+
+    context 'is surrounded by its own pieces' do
+      it 'returns 0 possible move locations' do
+        current_board = [
+          ["\u265F", "\u00B7", "\u265F", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u265C", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u265F", "\u00B7", "\u265F", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"]
+        ]
+        board_double = double('board_double', return_board: current_board)
+        own_piece_adjusted_moves = [
+          [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]
+        ]
+        expected_return = []
+        returned_array = bishop.piece_specific_restrictions(board_double, own_piece_adjusted_moves)
+
+        expect(returned_array).to eq(expected_return)
+        expect(returned_array.length).to eq(0)
+      end
+    end
+
+    context 'its own pieces are at the end of each row and column' do
+      it 'returns 5 possible move locations' do
+        current_board = [
+          ["\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F"],
+          ["\u265F", "\u265C", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u265F"],
+          ["\u265F", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u265F"],
+          ["\u265F", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u265F"],
+          ["\u265F", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u265F"],
+          ["\u265F", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u265F"],
+          ["\u265F", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u265F"],
+          ["\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F"],
+        ]
+        board_double = double('board_double', return_board: current_board)
+        own_piece_adjusted_moves = [
+          [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]
+        ]
+        expected_return = [
+          [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]
+        ]
+        returned_array = bishop.piece_specific_restrictions(board_double, own_piece_adjusted_moves)
+
+        expect(returned_array).to eq(expected_return)
+        expect(returned_array.length).to eq(5)
+      end
+    end
+
+    context 'is surrounded by the other players pieces' do
+      it 'returns 4 possible move locations' do
+        current_board = [
+          ["\u2659", "\u00B7", "\u2659", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u265C", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u2659", "\u00B7", "\u2659", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"]
+        ]
+        board_double = double('board_double', return_board: current_board)
+        own_piece_adjusted_moves = [
+          [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+          [0, 0],
+          [2, 0],
+          [0, 2]
+        ]
+        expected_return = [[2, 2], [0, 0], [2, 0], [0, 2]]
+        returned_array = bishop.piece_specific_restrictions(board_double, own_piece_adjusted_moves)
+
+        expect(returned_array).to eq(expected_return)
+        expect(returned_array.length).to eq(4)
+      end
+    end
+
+    context 'the other players pieces are at the end of each row and column' do
+      it 'returns 9 possible move locations' do
+        current_board = [
+          ["\u2659", "\u00B7", "\u2659", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u265C", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u2659", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7"],
+          ["\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u00B7", "\u2659"]
+        ]
+        board_double = double('board_double', return_board: current_board)
+        own_piece_adjusted_moves = [
+          [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+          [0, 0],
+          [2, 0],
+          [0, 2]
+        ]
+        returned_array = bishop.piece_specific_restrictions(board_double, own_piece_adjusted_moves)
+
+        expect(returned_array).to eq(own_piece_adjusted_moves)
+        expect(returned_array.length).to eq(9)
       end
     end
   end
